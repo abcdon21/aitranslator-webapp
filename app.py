075831ea13f82cf69118105b280802d851608a78
abcdon21 <abhishigupta21@gmail.com>
2026-04-05 14:28:51 +0530

    project ready

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from transformers import MarianMTModel, MarianTokenizer 
 
 app= Flask(__name__)
 
-@app.route('/')
+# to select the model
+
+model_name = "Helsinki-NLP/opus-mt-en-hi"
+
+#to load the model
+
+model= MarianMTModel.from_pretrained(model_name)
+
+#to load the tokenizer
+
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+
+# process of converting the input into numerical format is called tokenization
+
+def translation(data):
+    #converte the input text into tokens
+    inputs  = tokenizer(data, return_tensors="pt", padding=True)
+    #translate the data using model
+    translated = model.generate(**inputs)
+    #decode the token into readable text
+    result = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return result
+    
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    translated_text = ""
+    if request.method == 'POST':
+        data=request.form['data']
+        translated_text= translation(data)
+        
+        
+        
+        
+    return render_template('index.html', translated_text=translated_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
